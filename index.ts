ace2d136bdde8d2549e333e24ff72e199797e33c
rster2002 <rster2002dev@gmail.com>
2026-01-02 17:54:39 +0100

    👷 Working on CI

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 export { default as Vault } from "./src/classes/Vault.ts";
 export { default as Asset } from "./src/classes/Asset.ts";
+export { default as AssetView } from "./src/classes/AssetView.ts";
 export { default as MdContent } from "./src/classes/MdContent.ts";
 export { default as Link } from "./src/classes/Link.ts";
 export { default as Exporter } from "./src/classes/Exporter.ts";
+export { default as AutoExporter } from "./src/classes/AutoExporter.ts";
 
 export { default as autoWranglerArgs } from "./src/functions/autoWranglerArgs.ts";
 export type { default as WranglerArgs } from "./src/types/WranglerArgs.ts";
+export type { AutoExporterArgs } from "./src/schemas/autoExporterArgs.ts";
+export type { VaultArgs } from "./src/schemas/vaultArgs.ts";
